713987e353d1730814d270defdd11998223bc288
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-03 13:05:42 -0700

    adjust to use modules, compiled protos

--- system/src/lib.rs
+++ system/src/lib.rs
@@ -1,1 +1,12 @@
-// unimplemented!()
+pub mod external_client_grpc_server;
+pub mod internal_cluster_grpc_client;
+pub mod internal_cluster_grpc_server;
+pub mod server;
+
+pub(crate) mod meridian_client_v010 {
+    include!("../../proto/meridian.client.v010.rs");
+}
+
+pub(crate) mod meridian_cluster_v010 {
+    include!("../../proto/meridian.cluster.v010.rs");
+}
