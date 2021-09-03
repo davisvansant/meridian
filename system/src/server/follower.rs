8c4824994e9f192e3fc4fcfcaafb6f987e0f9d96
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-03 13:03:37 -0700

    initial server follower, tests

---
+++ system/src/server/follower.rs
@@ -0,0 +1,37 @@
+use tokio::time::Duration;
+
+pub struct Follower {
+    pub election_timeout: Duration,
+}
+
+impl Follower {
+    pub async fn init() -> Result<Follower, Box<dyn std::error::Error>> {
+        let election_timeout = Duration::from_millis(150);
+
+        Ok(Follower { election_timeout })
+    }
+
+    pub async fn election_timeout(&self) -> Result<(), Box<dyn std::error::Error>> {
+        println!("Waiting for leader...");
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init() -> Result<(), Box<dyn std::error::Error>> {
+        let test_follower = Follower::init().await?;
+        assert_eq!(test_follower.election_timeout.as_millis(), 150);
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn election_timeout() -> Result<(), Box<dyn std::error::Error>> {
+        let test_follower = Follower::init().await?;
+        assert!(test_follower.election_timeout().await.is_ok());
+        Ok(())
+    }
+}
