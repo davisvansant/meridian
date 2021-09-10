b3e4103271c6f35168311dd064a2fef182cf5a35
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-10 14:59:04 -0700

    add new enum

--- system/src/lib.rs
+++ system/src/lib.rs
@@ -2,6 +2,7 @@ pub mod external_client_grpc_server;
 pub mod internal_cluster_grpc_client;
 pub mod internal_cluster_grpc_server;
 pub mod server;
+pub mod state;
 
 pub(crate) mod meridian_client_v010 {
     include!("../../proto/meridian.client.v010.rs");
@@ -9,4 +10,17 @@ pub(crate) mod meridian_client_v010 {
 
 pub(crate) mod meridian_cluster_v010 {
     include!("../../proto/meridian.cluster.v010.rs");
+}
+
+use crate::meridian_cluster_v010::{
+    AppendEntriesRequest, AppendEntriesResponse, InstallSnapshotRequest, InstallSnapshotResponse,
+    RequestVoteRequest, RequestVoteResponse,
+};
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Actions {
+    AppendEntriesRequest(AppendEntriesRequest),
+    AppendEntriesResponse(AppendEntriesResponse),
+    RequestVoteRequest(RequestVoteRequest),
+    RequestVoteResponse(RequestVoteResponse),
 }
