0054c900b92b40ea776dbdaac4feb52f208601e6
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-10 14:58:36 -0700

    cleanup server, add new send/receive functionality, prep for future impl

--- system/src/server.rs
+++ system/src/server.rs
@@ -1,16 +1,14 @@
+use tokio::sync::broadcast::Sender;
 use tokio::time::timeout;
 
 use crate::server::candidate::Candidate;
 use crate::server::follower::Follower;
 use crate::server::leader::Leader;
-use crate::server::persistent_state::PersistentState;
-use crate::server::volatile_state::VolatileState;
+use crate::Actions;
 
-mod candidate;
-mod follower;
-mod leader;
-mod persistent_state;
-mod volatile_state;
+pub mod candidate;
+pub mod follower;
+pub mod leader;
 
 #[derive(Debug, PartialEq)]
 pub enum ServerState {
@@ -21,21 +19,54 @@ pub enum ServerState {
 
 pub struct Server {
     pub server_state: ServerState,
-    persistent_state: PersistentState,
-    volatile_state: VolatileState,
+    receive_actions: Sender<Actions>,
+    send_actions: Sender<Actions>,
 }
 
 impl Server {
-    pub async fn init() -> Result<Server, Box<dyn std::error::Error>> {
+    pub async fn init(
+        receive_actions: Sender<Actions>,
+        send_actions: Sender<Actions>,
+    ) -> Result<Server, Box<dyn std::error::Error>> {
         let server_state = ServerState::Follower;
-        let persistent_state = PersistentState::init().await?;
-        let volatile_state = VolatileState::init().await?;
 
         Ok(Server {
             server_state,
-            persistent_state,
-            volatile_state,
+            receive_actions,
+            send_actions,
         })
+    }
+
+    pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        while self.server_state == ServerState::Follower {
+            println!("doing follwer stuff!");
+
+            if let Err(error) = self.follower().await {
+                println!("Something went wrong with the follower - {:?}", error);
+            }
+
+            println!("transitioning to candidate...");
+        }
+
+        while self.server_state == ServerState::Candidate {
+            println!("doing candidate stuff!");
+
+            if let Err(error) = self.candidate().await {
+                println!("something went wrong with the candidate {:?}", error);
+            }
+
+            println!("transitioning to leader...");
+        }
+
+        if self.server_state == ServerState::Leader {
+            println!("Leader!");
+
+            if let Err(error) = self.leader().await {
+                println!("the leader had an error - {:?}", error);
+            }
+        }
+
+        Ok(())
     }
 
     pub async fn follower(&mut self) -> Result<(), Box<dyn std::error::Error>> {
@@ -54,28 +85,29 @@ impl Server {
     pub async fn candidate(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let candidate = Candidate::init().await?;
 
-        self.increment_current_term().await?;
+        // self.increment_current_term().await?;
 
-        let request = self.build_request_vote_request().await?;
-        let start_election = candidate.start_election(request);
+        // let request = self.build_request_vote_request().await?;
+        // let start_election = candidate.start_election(request);
 
-        if timeout(candidate.election_timeout, start_election)
-            .await
-            .is_ok()
-        {
-            self.server_state = ServerState::Leader;
-        }
+        // if timeout(candidate.election_timeout, start_election)
+        //     .await
+        //     .is_ok()
+        // {
+        //     self.server_state = ServerState::Leader;
+        // }
+        self.server_state = ServerState::Leader;
 
         Ok(())
     }
 
     pub async fn leader(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let leader = Leader::init().await?;
-        let request = self
-            .build_append_entires_request(&leader.volatile_state)
-            .await?;
-
-        leader.send_heartbeat(request).await?;
+        // let request = self
+        //     .build_append_entires_request(&leader.volatile_state)
+        //     .await?;
+        //
+        // leader.send_heartbeat(request).await?;
 
         Ok(())
     }
@@ -106,16 +138,20 @@ mod tests {
         Ok(())
     }
 
-    #[tokio::test(flavor = "multi_thread")]
-    async fn init() -> Result<(), Box<dyn std::error::Error>> {
-        let test_server = Server::init().await?;
-        assert_eq!(test_server.server_state, ServerState::Follower);
-        assert_eq!(test_server.persistent_state.current_term, 0);
-        assert_eq!(test_server.persistent_state.voted_for, None);
-        assert_eq!(test_server.persistent_state.log.len(), 0);
-        assert_eq!(test_server.persistent_state.log.capacity(), 4096);
-        assert_eq!(test_server.volatile_state.commit_index, 0);
-        assert_eq!(test_server.volatile_state.last_applied, 0);
-        Ok(())
-    }
+    // #[tokio::test(flavor = "multi_thread")]
+    // async fn init() -> Result<(), Box<dyn std::error::Error>> {
+    //     // let (test_sender, _) = test_channel().await;
+    //     // let (test_subscriber, _) = test_channel().await;
+    //     // let test_subscriber_clone = test_subscriber.clone();
+    //     // let test_server = Server::init(test_subscriber, test_subscriber_clone).await?;
+    //     let test_server = test_server().await?;
+    //     // assert_eq!(test_server.server_state, ServerState::Follower);
+    //     assert_eq!(test_server.persistent_state.current_term, 0);
+    //     assert_eq!(test_server.persistent_state.voted_for, None);
+    //     assert_eq!(test_server.persistent_state.log.len(), 0);
+    //     assert_eq!(test_server.persistent_state.log.capacity(), 4096);
+    //     assert_eq!(test_server.volatile_state.commit_index, 0);
+    //     assert_eq!(test_server.volatile_state.last_applied, 0);
+    //     Ok(())
+    // }
 }
