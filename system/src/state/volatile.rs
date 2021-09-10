88b59aefda044ba41ece59a657d2ced72e7df133
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-10 14:48:29 -0700

    initial volatile module, tests

--- system/src/state/volatile.rs
+++ system/src/state/volatile.rs
@@ -1,14 +1,14 @@
-pub struct VolatileState {
+pub struct Volatile {
     pub commit_index: u32,
     pub last_applied: u32,
 }
 
-impl VolatileState {
-    pub async fn init() -> Result<VolatileState, Box<dyn std::error::Error>> {
+impl Volatile {
+    pub async fn init() -> Result<Volatile, Box<dyn std::error::Error>> {
         let commit_index = 0;
         let last_applied = 0;
 
-        Ok(VolatileState {
+        Ok(Volatile {
             commit_index,
             last_applied,
         })
@@ -21,7 +21,7 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn init() -> Result<(), Box<dyn std::error::Error>> {
-        let test_volatile_state = VolatileState::init().await?;
+        let test_volatile_state = Volatile::init().await?;
         assert_eq!(test_volatile_state.commit_index, 0);
         assert_eq!(test_volatile_state.last_applied, 0);
         Ok(())
