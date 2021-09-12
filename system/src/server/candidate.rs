413cb6403e0e8b3b30c8e45dc1b93bccd2994c59
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-11 17:25:00 -0700

    cleanup

--- system/src/server/candidate.rs
+++ system/src/server/candidate.rs
@@ -2,7 +2,6 @@ use tokio::time::Duration;
 
 use crate::internal_cluster_grpc_client::InternalClusterGrpcClient;
 use crate::meridian_cluster_v010::RequestVoteRequest;
-use crate::server::Server;
 
 pub struct Candidate {
     pub election_timeout: Duration,
@@ -23,38 +22,6 @@ impl Candidate {
     }
 }
 
-// impl Server {
-//     pub(super) async fn increment_current_term(
-//         &mut self,
-//     ) -> Result<(), Box<dyn std::error::Error>> {
-//         self.persistent_state.current_term += 1;
-//
-//         Ok(())
-//     }
-//
-//     pub(super) async fn build_request_vote_request(
-//         &self,
-//     ) -> Result<RequestVoteRequest, Box<dyn std::error::Error>> {
-//         let term = self.persistent_state.current_term;
-//         let candidate_id = String::from("some_candidate_id");
-//         let last_log_index = self.persistent_state.log.len() as u32;
-//         let last_log_term = if let Some(log) = self.persistent_state.log.last() {
-//             log.term
-//         } else {
-//             0
-//         };
-//
-//         let request_vote_request = RequestVoteRequest {
-//             term,
-//             candidate_id,
-//             last_log_index,
-//             last_log_term,
-//         };
-//
-//         Ok(request_vote_request)
-//     }
-// }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,28 +40,6 @@ mod tests {
     //     let test_server = test_server().await?;
     //     let test_request = test_server.build_request_vote_request().await?;
     //     assert!(test_candidate.start_election(test_request).await.is_ok());
-    //     Ok(())
-    // }
-
-    // #[tokio::test(flavor = "multi_thread")]
-    // async fn increment_current_term() -> Result<(), Box<dyn std::error::Error>> {
-    //     // let mut test_server = Server::init().await?;
-    //     let mut test_server = test_server().await?;
-    //     assert_eq!(test_server.persistent_state.current_term, 0);
-    //     test_server.increment_current_term().await?;
-    //     assert_eq!(test_server.persistent_state.current_term, 1);
-    //     Ok(())
-    // }
-    //
-    // #[tokio::test(flavor = "multi_thread")]
-    // async fn build_request_vote_request() -> Result<(), Box<dyn std::error::Error>> {
-    //     // let test_server = Server::init().await?;
-    //     let test_server = test_server().await?;
-    //     let test_request = test_server.build_request_vote_request().await?;
-    //     assert_eq!(test_request.term, 0);
-    //     assert_eq!(test_request.candidate_id.as_str(), "some_candidate_id");
-    //     assert_eq!(test_request.last_log_index, 0);
-    //     assert_eq!(test_request.last_log_term, 0);
     //     Ok(())
     // }
 }
