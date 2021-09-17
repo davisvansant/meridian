b64e71b9581de7ccbdf0987f2e514027000d78aa
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-17 13:17:29 -0700

    initial use of new mods

--- system/src/lib.rs
+++ system/src/lib.rs
@@ -1,6 +1,9 @@
 pub mod external_client_grpc_server;
+pub mod external_membership_grpc_server;
 pub mod internal_cluster_grpc_client;
 pub mod internal_cluster_grpc_server;
+pub mod membership;
+pub mod node;
 pub mod server;
 pub mod state;
 
@@ -12,10 +15,16 @@ pub(crate) mod meridian_cluster_v010 {
     include!("../../proto/meridian.cluster.v010.rs");
 }
 
+pub(crate) mod meridian_membership_v010 {
+    include!("../../proto/meridian.membership.v010.rs");
+}
+
 use crate::meridian_cluster_v010::{
     AppendEntriesRequest, AppendEntriesResponse, InstallSnapshotRequest, InstallSnapshotResponse,
     RequestVoteRequest, RequestVoteResponse,
 };
+
+use crate::meridian_membership_v010::{JoinClusterRequest, JoinClusterResponse};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Actions {
