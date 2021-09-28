d8285c413a98ea9169a291e67e5e14f125cfb190
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-09-27 18:46:13 -0700

    add initial membership client impl

---
+++ system/src/external_membership_grpc_client.rs
@@ -0,0 +1,29 @@
+use tonic::transport::Channel;
+use tonic::{Request, Response, Status};
+
+pub use crate::meridian_membership_v010::communications_client::CommunicationsClient;
+use crate::meridian_membership_v010::{JoinClusterRequest, JoinClusterResponse};
+
+pub struct ExternalMembershipGrpcClient {
+    transport: CommunicationsClient<Channel>,
+}
+
+impl ExternalMembershipGrpcClient {
+    pub async fn init(
+        endpoint: String,
+    ) -> Result<ExternalMembershipGrpcClient, Box<dyn std::error::Error>> {
+        let transport = CommunicationsClient::connect(endpoint).await?;
+
+        Ok(ExternalMembershipGrpcClient { transport })
+    }
+
+    pub async fn join_cluster(
+        &mut self,
+        request: JoinClusterRequest,
+    ) -> Result<Response<JoinClusterResponse>, Status> {
+        let mut transport = self.transport.clone();
+        let response = transport.join_cluster(request).await?;
+
+        Ok(response)
+    }
+}
