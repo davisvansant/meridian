2b04ecd16888c105286cb449a8c9fa09bdb8c0d8
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-10-07 13:33:20 -0700

    add new client calls, adjusting to use new rpc services

--- system/src/grpc/membership_client.rs
+++ system/src/grpc/membership_client.rs
@@ -1,6 +1,6 @@
 use crate::grpc::{Channel, Response, Status};
 pub use crate::meridian_membership_v010::communications_client::CommunicationsClient;
-use crate::meridian_membership_v010::{JoinClusterRequest, JoinClusterResponse};
+use crate::meridian_membership_v010::{Empty, MembershipNode, NodeStatus, Nodes};
 
 pub struct ExternalMembershipGrpcClient {
     transport: CommunicationsClient<Channel>,
@@ -17,10 +17,26 @@ impl ExternalMembershipGrpcClient {
 
     pub async fn join_cluster(
         &mut self,
-        request: JoinClusterRequest,
-    ) -> Result<Response<JoinClusterResponse>, Status> {
+        request: MembershipNode,
+    ) -> Result<Response<MembershipNode>, Status> {
         let mut transport = self.transport.clone();
         let response = transport.join_cluster(request).await?;
+
+        Ok(response)
+    }
+
+    pub async fn get_nodes(&mut self) -> Result<Response<Nodes>, Status> {
+        let mut transport = self.transport.clone();
+        let request = Empty {};
+        let response = transport.get_nodes(request).await?;
+
+        Ok(response)
+    }
+
+    pub async fn get_node_status(&mut self) -> Result<Response<NodeStatus>, Status> {
+        let mut transport = self.transport.clone();
+        let request = Empty {};
+        let response = transport.get_node_status(request).await?;
 
         Ok(response)
     }
