c9ab3152e6d5fb010e77c8414927daf3bd725d86
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-10-07 13:39:28 -0700

    remove launch channel support

--- system/src/runtime/tasks/membership_service.rs
+++ system/src/runtime/tasks/membership_service.rs
@@ -4,25 +4,24 @@ use crate::node::Node;
 use crate::runtime::sync::launch::ChannelLaunch;
 use crate::runtime::sync::membership_receive_task::ChannelMembershipReceiveTask;
 use crate::runtime::sync::membership_send_grpc_task::ChannelMembershipSendGrpcTask;
+use crate::runtime::sync::membership_send_preflight_task::ChannelMembershipSendPreflightTask;
 use crate::runtime::sync::membership_send_server_task::ChannelMembershipSendServerTask;
 use crate::runtime::tasks::JoinHandle;
 
 pub async fn run_task(
     cluster_size: ClusterSize,
     server: Node,
-    peers: Vec<String>,
     membership_send_grpc_task: ChannelMembershipReceiveTask,
     membership_receive_grpc_task: ChannelMembershipSendGrpcTask,
+    membership_send_preflight_task: ChannelMembershipSendPreflightTask,
     membership_send_server_task: ChannelMembershipSendServerTask,
-    launch_action: ChannelLaunch,
 ) -> Result<JoinHandle<()>, Box<dyn std::error::Error>> {
-    // let server = self.to_owned();
     let mut membership = Membership::init(
-        // ClusterSize::One,
         cluster_size,
         server,
         membership_send_grpc_task,
         membership_receive_grpc_task,
+        membership_send_preflight_task,
         membership_send_server_task,
     )
     .await?;
@@ -30,7 +29,7 @@ pub async fn run_task(
     let membership_run_handle = tokio::spawn(async move {
         // sleep(Duration::from_secs(10)).await;
 
-        if let Err(error) = membership.run(peers, launch_action).await {
+        if let Err(error) = membership.run().await {
             println!("error with running {:?}", error);
         };
     });
