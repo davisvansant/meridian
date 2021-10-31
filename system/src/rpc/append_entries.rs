9dea366ee362db0a81d4384b397b24ab3bb387b0
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-10-31 15:29:52 -0700

    adjust struct field to string, add impls, update tests

--- system/src/rpc/append_entries.rs
+++ system/src/rpc/append_entries.rs
@@ -1,15 +1,44 @@
-pub struct Arguments<'a> {
+pub struct Arguments {
     term: u8,
-    leader_id: &'a str,
+    leader_id: String,
     prev_log_index: u8,
     prev_log_term: u8,
     entries: Vec<u8>,
     leader_commit: u8,
 }
 
+impl Arguments {
+    pub async fn build() -> Result<Arguments, Box<dyn std::error::Error>> {
+        let term = 0;
+        let leader_id = String::from("some_leader_id");
+        let prev_log_index = 0;
+        let prev_log_term = 0;
+        let entries = Vec::with_capacity(10);
+        let leader_commit = 0;
+
+        Ok(Arguments {
+            term,
+            leader_id,
+            prev_log_index,
+            prev_log_term,
+            entries,
+            leader_commit,
+        })
+    }
+}
+
 pub struct Results {
     term: u8,
     success: bool,
+}
+
+impl Results {
+    pub async fn build() -> Result<Results, Box<dyn std::error::Error>> {
+        let term = 0;
+        let success = false;
+
+        Ok(Results { term, success })
+    }
 }
 
 #[cfg(test)]
@@ -18,31 +47,25 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn arguments() -> Result<(), Box<dyn std::error::Error>> {
-        let test_arguments = Arguments {
-            term: 0,
-            leader_id: "some_leader_id",
-            prev_log_index: 0,
-            prev_log_term: 0,
-            entries: Vec::with_capacity(0),
-            leader_commit: 0,
-        };
+        let test_arguments = Arguments::build().await?;
+
         assert_eq!(test_arguments.term, 0);
         assert_eq!(test_arguments.leader_id, "some_leader_id");
         assert_eq!(test_arguments.prev_log_index, 0);
         assert_eq!(test_arguments.prev_log_term, 0);
         assert_eq!(test_arguments.entries.len(), 0);
         assert_eq!(test_arguments.leader_commit, 0);
+
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
     async fn results() -> Result<(), Box<dyn std::error::Error>> {
-        let test_results = Results {
-            term: 0,
-            success: false,
-        };
+        let test_results = Results::build().await?;
+
         assert_eq!(test_results.term, 0);
         assert!(!test_results.success);
+
         Ok(())
     }
 }
