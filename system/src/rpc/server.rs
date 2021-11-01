d1b465a842ee13ee54bc758e37167f5959f4b9ad
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-11-01 13:08:38 -0700

    add initial route_incomming() impl, tests

--- system/src/rpc/server.rs
+++ system/src/rpc/server.rs
@@ -1,3 +1,4 @@
+use flexbuffers::{Builder, BuilderOptions, Pushable};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
@@ -58,6 +59,32 @@ impl Server {
         }
 
         Ok(())
+    }
+
+    async fn route_incoming(&self, data: &[u8]) -> Result<String, Box<dyn std::error::Error>> {
+        let mut flexbuffers_builder = Builder::new(BuilderOptions::SHARE_NONE);
+
+        data.push_to_builder(&mut flexbuffers_builder);
+
+        let flexbuffers_root = flexbuffers::Reader::get_root(flexbuffers_builder.view())?;
+
+        match flexbuffers_root.as_map().idx("data").as_str() {
+            "append_entries_arguments" => {
+                println!("received append entries arguments!");
+
+                Ok(String::from("append_entries_arguments"))
+            }
+            "request_vote_arguments" => {
+                println!("received request vote arguments!");
+
+                Ok(String::from("request_vote_arguments"))
+            }
+            _ => {
+                println!("currently unknown ...");
+
+                Ok(String::from("unknown"))
+            }
+        }
     }
 }
 
@@ -149,6 +176,44 @@ mod tests {
 
         assert_eq!(test_data.as_str(), "test_rpc_membership_interface");
         assert!(test_handle.await.is_ok());
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn route_incoming_append_entries() -> Result<(), Box<dyn std::error::Error>> {
+        let test_membership_server = Server::init(Interface::Membership).await?;
+        let test_append_entries_arguments =
+            crate::rpc::Data::AppendEntriesArguments.build().await?;
+        let test_data = test_membership_server
+            .route_incoming(&test_append_entries_arguments)
+            .await?;
+
+        assert_eq!(test_data.as_str(), "append_entries_arguments");
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn route_incoming_request_vote() -> Result<(), Box<dyn std::error::Error>> {
+        let test_membership_server = Server::init(Interface::Membership).await?;
+        let test_request_vote_arguments = crate::rpc::Data::RequestVoteArguments.build().await?;
+        let test_data = test_membership_server
+            .route_incoming(&test_request_vote_arguments)
+            .await?;
+
+        assert_eq!(test_data.as_str(), "request_vote_arguments");
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn route_incoming_unknown() -> Result<(), Box<dyn std::error::Error>> {
+        let test_membership_server = Server::init(Interface::Membership).await?;
+        let test_unknown = crate::rpc::Data::RequestVoteResults.build().await?;
+        let test_data = test_membership_server.route_incoming(&test_unknown).await?;
+
+        assert_eq!(test_data.as_str(), "unknown");
 
         Ok(())
     }
