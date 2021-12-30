9a7d3c833a696f2181128fe9a790c957e455684f
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-30 10:33:01 -0800

    adjust impl to handle initial rpc calls

--- system/src/rpc/client.rs
+++ system/src/rpc/client.rs
@@ -6,6 +6,24 @@ use crate::rpc::build_ip_address;
 use crate::rpc::build_socket_address;
 use crate::rpc::Interface;
 
+use flexbuffers::singleton;
+
+use crate::rpc::Node;
+
+use crate::rpc::Data;
+
+use std::str::FromStr;
+
+use flexbuffers::Pushable;
+
+use flexbuffers::{Builder, BuilderOptions};
+
+use uuid::Uuid;
+
+use crate::rpc::membership::Connected;
+
+use crate::rpc::membership::MembershipNode;
+
 pub struct Client {
     ip_address: IpAddr,
     port: u16,
@@ -27,6 +45,120 @@ impl Client {
             port,
             socket_address,
         })
+    }
+
+    pub async fn join_cluster(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let test_node_address = std::net::IpAddr::from_str("0.0.0.0")?;
+        let test_node = Node::init(test_node_address, 10000, 15000, 20000).await?;
+        let request = Data::JoinClusterRequest(test_node).build().await?;
+        let response = self.transmit(&request).await?;
+
+        let mut flexbuffers_builder = Builder::new(BuilderOptions::SHARE_NONE);
+
+        response.push_to_builder(&mut flexbuffers_builder);
+
+        let flexbuffer_root = flexbuffers::Reader::get_root(flexbuffers_builder.view())?;
+
+        // let test_flexbuffers_root_details = test_flexbuffer_root.as_map().idx("details").as_map();
+        // let flexbuffer = singleton(response);
+        // let
+        // let remote_node = singleton(&response);
+        // let root = remote_node.get_root()?;
+
+        // // println!("response -> {:?}", String::from_utf8(response)?);
+        // for value in remote_node.as_map().idx("details").as_map().iter_values() {
+        //     println!("some value here - {:?}", value.as_str());
+        // }
+        // for key in flexbuffer_root.as_map().idx("details").as_map().iter_keys() {
+        //     println!("some keys from the buffer -> {:?}", key)
+        // }
+
+        // for value in flexbuffer_root.as_map().idx("details").as_map().iter_values() {
+        //     println!("some values from the buffer -> {:?}", value.as_str())
+        // }
+
+        Ok(())
+    }
+
+    pub async fn get_connected(&self) -> Result<Vec<MembershipNode>, Box<dyn std::error::Error>> {
+        let request = Data::ConnectedRequest.build().await?;
+        let response = self.transmit(&request).await?;
+
+        let mut another_test_flexbuffers_builder = Builder::new(BuilderOptions::SHARE_NONE);
+
+        response.push_to_builder(&mut another_test_flexbuffers_builder);
+
+        let another_test_flexbuffer_root =
+            flexbuffers::Reader::get_root(another_test_flexbuffers_builder.view())?;
+
+        println!("Im still alive");
+
+        let flexbuffers_root = another_test_flexbuffer_root
+            .as_map()
+            .idx("details")
+            .as_map()
+            .idx("nodes")
+            .as_vector();
+        let mut connected = Connected::build().await?;
+
+        println!("{:?}", &flexbuffers_root.len());
+
+        println!("building the sclak");
+
+        match flexbuffers_root.is_empty() {
+            true => {
+                println!("empty nodes...");
+            }
+            false => {
+                for node in flexbuffers_root.iter() {
+                    println!("are we the errorrer");
+                    println!("{:?}", node.as_map().idx("id").as_str());
+                    let id =
+                        Uuid::parse_str(node.as_map().idx("details").as_map().idx("id").as_str())?;
+                    println!("are we happy?");
+                    let address = IpAddr::from_str(
+                        node.as_map()
+                            .idx("details")
+                            .as_map()
+                            .idx("address")
+                            .as_str(),
+                    )?;
+                    println!("are we dead?");
+                    let client_port = u16::from_str(
+                        node.as_map()
+                            .idx("details")
+                            .as_map()
+                            .idx("client_port")
+                            .as_str(),
+                    )?;
+                    let cluster_port = u16::from_str(
+                        node.as_map()
+                            .idx("details")
+                            .as_map()
+                            .idx("cluster_port")
+                            .as_str(),
+                    )?;
+                    let membership_port = u16::from_str(
+                        node.as_map()
+                            .idx("details")
+                            .as_map()
+                            .idx("membership_port")
+                            .as_str(),
+                    )?;
+
+                    let connected_node = MembershipNode {
+                        id: id.to_string(),
+                        address: address.to_string(),
+                        client_port: client_port.to_string(),
+                        cluster_port: cluster_port.to_string(),
+                        membership_port: membership_port.to_string(),
+                    };
+
+                    connected.nodes.push(connected_node);
+                }
+            }
+        }
+        Ok(connected.nodes)
     }
 
     pub async fn transmit(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
