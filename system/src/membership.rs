a654d49fde53227f22f42905538f56d180a112e7
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-30 10:34:06 -0800

    adjust to initial mpsc/oneshot impl

--- system/src/membership.rs
+++ system/src/membership.rs
@@ -16,6 +16,8 @@ use crate::runtime::sync::membership_send_preflight_task::MembershipSendPrefligh
 use crate::runtime::sync::membership_send_server_task::ChannelMembershipSendServerTask;
 use crate::runtime::sync::membership_send_server_task::MembershipSendServerTask;
 
+use crate::channel::{MembershipReceiver, MembershipRequest, MembershipResponse, MembershipSender};
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum ClusterSize {
     One,
@@ -33,7 +35,7 @@ impl ClusterSize {
     }
 }
 
-#[derive(Clone, Debug)]
+// #[derive(Clone, Debug)]
 pub struct Membership {
     cluster_size: ClusterSize,
     server: Node,
@@ -42,6 +44,7 @@ pub struct Membership {
     send_grpc_task: ChannelMembershipSendGrpcTask,
     send_preflight_task: ChannelMembershipSendPreflightTask,
     send_server_task: ChannelMembershipSendServerTask,
+    receiver: MembershipReceiver,
 }
 
 impl Membership {
@@ -52,6 +55,7 @@ impl Membership {
         send_grpc_task: ChannelMembershipSendGrpcTask,
         send_preflight_task: ChannelMembershipSendPreflightTask,
         send_server_task: ChannelMembershipSendServerTask,
+        receiver: MembershipReceiver,
     ) -> Result<Membership, Box<dyn std::error::Error>> {
         let members = cluster_size.members().await;
 
@@ -63,55 +67,67 @@ impl Membership {
             receive_task,
             send_preflight_task,
             send_server_task,
+            receiver,
         })
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut receiver = self.receive_task.subscribe();
+        // let mut receiver = self.receive_task.subscribe();
 
         println!("membership initialized and running...");
 
-        while let Ok(action) = receiver.recv().await {
-            match action {
-                MembershipReceiveTask::JoinCluster((service, membership_node)) => {
+        while let Some((request, response)) = self.receiver.recv().await {
+            match request {
+                MembershipRequest::JoinCluster(node) => {
                     println!("received join request");
 
-                    match service {
-                        1 => {
-                            let node = Self::build_node(membership_node).await?;
-                            match self.members.insert(node.id, node) {
-                                Some(value) => println!("updated node! {:?}", value),
-                                None => println!("added node !"),
-                            }
-                        }
-                        2 => {
-                            let response =
-                                self.action_join_cluster_request(membership_node).await?;
+                    // let node = Self::build_node(membership_node).await?;
 
-                            self.send_grpc_task
-                                .send(MembershipSendGrpcTask::JoinClusterResponse(response))?;
-                        }
-                        _ => panic!("received unexpected service"),
+                    match self.members.insert(node.id, node) {
+                        Some(value) => println!("updated node! {:?}", value),
+                        None => println!("added node !"),
                     }
-                }
-                MembershipReceiveTask::Node(service) => {
-                    println!("received node request!");
+                    // match service {
+                    //     1 => {
+                    //         let node = Self::build_node(membership_node).await?;
+                    //         match self.members.insert(node.id, node) {
+                    //             Some(value) => println!("updated node! {:?}", value),
+                    //             None => println!("added node !"),
+                    //         }
+                    //     }
+                    //     2 => {
+                    //         let response =
+                    //             self.action_join_cluster_request(membership_node).await?;
 
+                    //         self.send_grpc_task
+                    //             .send(MembershipSendGrpcTask::JoinClusterResponse(response))?;
+                    //     }
+                    //     _ => panic!("received unexpected service"),
+                    // }
+                }
+                MembershipRequest::Node => {
+                    // println!("received node request!");
+
+                    // let node = self.server;
+
+                    // match service {
+                    //     1 => {
+                    //         self.send_preflight_task
+                    //             .send(MembershipSendPreflightTask::NodeResponse(node))?;
+                    //     }
+                    //     2 => {
+                    //         self.send_server_task
+                    //             .send(MembershipSendServerTask::NodeResponse(node))?;
+                    //     }
+                    //     _ => panic!("received task from unexpected service ..."),
+                    // }
                     let node = self.server;
 
-                    match service {
-                        1 => {
-                            self.send_preflight_task
-                                .send(MembershipSendPreflightTask::NodeResponse(node))?;
-                        }
-                        2 => {
-                            self.send_server_task
-                                .send(MembershipSendServerTask::NodeResponse(node))?;
-                        }
-                        _ => panic!("received task from unexpected service ..."),
+                    if let Err(error) = response.send(MembershipResponse::Node(node)) {
+                        println!("error sending membership response -> {:?}", error);
                     }
                 }
-                MembershipReceiveTask::Members(service) => {
+                MembershipRequest::Members(service) => {
                     println!("received members request!");
 
                     let mut members = Vec::with_capacity(self.members.len());
@@ -131,41 +147,41 @@ impl Membership {
                         // members.dedup();
                     }
 
-                    match service {
-                        1 => {
-                            self.send_preflight_task
-                                .send(MembershipSendPreflightTask::MembersResponse(members))?;
-                        }
-                        2 => {
-                            self.send_server_task.send(
-                                MembershipSendServerTask::MembersResponse(members.to_vec()),
-                            )?;
-                        }
-                        3 => {
-                            let mut members = Vec::with_capacity(self.members.len());
+                    // match service {
+                    //     1 => {
+                    //         self.send_preflight_task
+                    //             .send(MembershipSendPreflightTask::MembersResponse(members))?;
+                    //     }
+                    //     2 => {
+                    //         self.send_server_task.send(
+                    //             MembershipSendServerTask::MembersResponse(members.to_vec()),
+                    //         )?;
+                    //     }
+                    //     3 => {
+                    //         let mut members = Vec::with_capacity(self.members.len());
 
-                            for m in self.members.values() {
-                                println!("peers !{:?}", m);
+                    //         for m in self.members.values() {
+                    //             println!("peers !{:?}", m);
 
-                                let node = MembershipNode {
-                                    id: m.id.to_string(),
-                                    address: m.address.to_string(),
-                                    client_port: m.client_port.to_string(),
-                                    cluster_port: m.cluster_port.to_string(),
-                                    membership_port: m.membership_port.to_string(),
-                                };
+                    //             let node = MembershipNode {
+                    //                 id: m.id.to_string(),
+                    //                 address: m.address.to_string(),
+                    //                 client_port: m.client_port.to_string(),
+                    //                 cluster_port: m.cluster_port.to_string(),
+                    //                 membership_port: m.membership_port.to_string(),
+                    //             };
 
-                                members.push(node);
-                                // members.dedup();
-                            }
-                            let nodes = Nodes { nodes: members };
-                            self.send_grpc_task
-                                .send(MembershipSendGrpcTask::Nodes(nodes))?;
-                        }
-                        _ => panic!("received task from unexpected service ..."),
-                    }
+                    //             members.push(node);
+                    //             // members.dedup();
+                    //         }
+                    //         let nodes = Nodes { nodes: members };
+                    //         self.send_grpc_task
+                    //             .send(MembershipSendGrpcTask::Nodes(nodes))?;
+                    //     }
+                    //     _ => panic!("received task from unexpected service ..."),
+                    // }
                 }
-                MembershipReceiveTask::Status => {
+                MembershipRequest::Status => {
                     let length = match self.members.len() {
                         0 => String::from("0"),
                         1 => String::from("1"),
@@ -184,6 +200,125 @@ impl Membership {
 
         Ok(())
     }
+
+    // pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    //     let mut receiver = self.receive_task.subscribe();
+
+    //     println!("membership initialized and running...");
+
+    //     while let Ok(action) = receiver.recv().await {
+    //         match action {
+    //             MembershipReceiveTask::JoinCluster((service, membership_node)) => {
+    //                 println!("received join request");
+
+    //                 match service {
+    //                     1 => {
+    //                         let node = Self::build_node(membership_node).await?;
+    //                         match self.members.insert(node.id, node) {
+    //                             Some(value) => println!("updated node! {:?}", value),
+    //                             None => println!("added node !"),
+    //                         }
+    //                     }
+    //                     2 => {
+    //                         let response =
+    //                             self.action_join_cluster_request(membership_node).await?;
+
+    //                         self.send_grpc_task
+    //                             .send(MembershipSendGrpcTask::JoinClusterResponse(response))?;
+    //                     }
+    //                     _ => panic!("received unexpected service"),
+    //                 }
+    //             }
+    //             MembershipReceiveTask::Node(service) => {
+    //                 println!("received node request!");
+
+    //                 let node = self.server;
+
+    //                 match service {
+    //                     1 => {
+    //                         self.send_preflight_task
+    //                             .send(MembershipSendPreflightTask::NodeResponse(node))?;
+    //                     }
+    //                     2 => {
+    //                         self.send_server_task
+    //                             .send(MembershipSendServerTask::NodeResponse(node))?;
+    //                     }
+    //                     _ => panic!("received task from unexpected service ..."),
+    //                 }
+    //             }
+    //             MembershipReceiveTask::Members(service) => {
+    //                 println!("received members request!");
+
+    //                 let mut members = Vec::with_capacity(self.members.len());
+
+    //                 for m in self.members.values() {
+    //                     println!("peers !{:?}", m);
+
+    //                     // let node = MembershipNode {
+    //                     //     id: m.id.to_string(),
+    //                     //     address: m.address.to_string(),
+    //                     //     client_port: m.client_port.to_string(),
+    //                     //     cluster_port: m.cluster_port.to_string(),
+    //                     //     membership_port: m.membership_port.to_string(),
+    //                     // };
+
+    //                     members.push(m.to_owned());
+    //                     // members.dedup();
+    //                 }
+
+    //                 match service {
+    //                     1 => {
+    //                         self.send_preflight_task
+    //                             .send(MembershipSendPreflightTask::MembersResponse(members))?;
+    //                     }
+    //                     2 => {
+    //                         self.send_server_task.send(
+    //                             MembershipSendServerTask::MembersResponse(members.to_vec()),
+    //                         )?;
+    //                     }
+    //                     3 => {
+    //                         let mut members = Vec::with_capacity(self.members.len());
+
+    //                         for m in self.members.values() {
+    //                             println!("peers !{:?}", m);
+
+    //                             let node = MembershipNode {
+    //                                 id: m.id.to_string(),
+    //                                 address: m.address.to_string(),
+    //                                 client_port: m.client_port.to_string(),
+    //                                 cluster_port: m.cluster_port.to_string(),
+    //                                 membership_port: m.membership_port.to_string(),
+    //                             };
+
+    //                             members.push(node);
+    //                             // members.dedup();
+    //                         }
+    //                         let nodes = Nodes { nodes: members };
+    //                         self.send_grpc_task
+    //                             .send(MembershipSendGrpcTask::Nodes(nodes))?;
+    //                     }
+    //                     _ => panic!("received task from unexpected service ..."),
+    //                 }
+    //             }
+    //             MembershipReceiveTask::Status => {
+    //                 let length = match self.members.len() {
+    //                     0 => String::from("0"),
+    //                     1 => String::from("1"),
+    //                     2 => String::from("2"),
+    //                     3 => String::from("3"),
+    //                     _ => panic!("unexpected number of peers!"),
+    //                 };
+
+    //                 let status = NodeStatus { status: length };
+
+    //                 self.send_grpc_task
+    //                     .send(MembershipSendGrpcTask::Status(status))?;
+    //             }
+    //         }
+    //     }
+
+    //     Ok(())
+    // }
 
     async fn action_join_cluster_request(
         &mut self,
@@ -207,6 +342,7 @@ impl Membership {
         Ok(response)
     }
 
+    // async fn build_node(node: MembershipNode) -> Result<Node, Box<dyn std::error::Error>> {
     async fn build_node(node: MembershipNode) -> Result<Node, Box<dyn std::error::Error>> {
         let id = Uuid::from_str(&node.id)?;
         let address = IpAddr::from_str(&node.address)?;
