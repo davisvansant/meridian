aefb9defde4a3a0e93b35bdd751179750bb3e2c2
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-03 09:50:38 -0800

    adjust channel impl, initial use of mods

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -1,55 +1,14 @@
-use tokio::sync::{mpsc, oneshot};
+mod client;
+mod membership;
+mod server;
+mod state;
 
-use crate::node::Node;
+pub use client::{ClientReceiver, ClientRequest, ClientResponse, ClientSender};
+pub use membership::{MembershipReceiver, MembershipRequest, MembershipResponse, MembershipSender};
+pub use server::{CandidateReceiver, CandidateSender, CandidateTransition};
+pub use server::{ServerReceiver, ServerSender, ServerState};
+pub use state::{StateReceiver, StateRequest, StateResponse, StateSender};
 
-pub type MembershipReceiver =
-    mpsc::Receiver<(MembershipRequest, oneshot::Sender<MembershipResponse>)>;
-pub type MembershipSender = mpsc::Sender<(MembershipRequest, oneshot::Sender<MembershipResponse>)>;
-
-#[derive(Clone, Debug)]
-pub enum MembershipRequest {
-    JoinCluster(Node),
-    Node,
-    Members(u8),
-    Status,
-}
-
-#[derive(Clone, Debug)]
-pub enum MembershipResponse {
-    JoinCluster(Node),
-    Node(Node),
-    Members(u8),
-    Status,
-}
-
-pub async fn join_cluster(
-    membership: &MembershipSender,
-    node: Node,
-) -> Result<Node, Box<dyn std::error::Error>> {
-    let (request, response) = oneshot::channel();
-
-    membership
-        .send((MembershipRequest::JoinCluster(node), request))
-        .await?;
-
-    // let node = response.await?;
-
-    // Ok(node)
-    match response.await {
-        Ok(MembershipResponse::JoinCluster(node)) => Ok(node),
-        Err(error) => Err(Box::new(error)),
-        _ => panic!("unexpected response!"),
-    }
-}
-
-pub async fn get_node(membership: &MembershipSender) -> Result<Node, Box<dyn std::error::Error>> {
-    let (request, response) = oneshot::channel();
-
-    membership.send((MembershipRequest::Node, request)).await?;
-
-    match response.await {
-        Ok(MembershipResponse::Node(node)) => Ok(node),
-        Err(error) => Err(Box::new(error)),
-        _ => panic!("unexpected response!"),
-    }
-}
+pub use client::{send_heartbeat, start_election};
+pub use membership::get_node;
+pub use state::{append_entries, candidate, leader, request_vote};
