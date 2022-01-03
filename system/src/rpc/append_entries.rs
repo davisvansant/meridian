99483b0e72eafac62d0ccb612a603f217cc1127d
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-03 09:55:13 -0800

    adjust struct name, adjust field types to u32

--- system/src/rpc/append_entries.rs
+++ system/src/rpc/append_entries.rs
@@ -1,14 +1,15 @@
-pub struct Arguments {
-    pub term: u8,
+#[derive(Clone, Debug)]
+pub struct AppendEntriesArguments {
+    pub term: u32,
     pub leader_id: String,
-    pub prev_log_index: u8,
-    pub prev_log_term: u8,
-    pub entries: Vec<u8>,
-    pub leader_commit: u8,
+    pub prev_log_index: u32,
+    pub prev_log_term: u32,
+    pub entries: Vec<u32>,
+    pub leader_commit: u32,
 }
 
-impl Arguments {
-    pub async fn build() -> Result<Arguments, Box<dyn std::error::Error>> {
+impl AppendEntriesArguments {
+    pub async fn build() -> Result<AppendEntriesArguments, Box<dyn std::error::Error>> {
         let term = 0;
         let leader_id = String::from("some_leader_id");
         let prev_log_index = 0;
@@ -16,7 +17,7 @@ impl Arguments {
         let entries = Vec::with_capacity(10);
         let leader_commit = 0;
 
-        Ok(Arguments {
+        Ok(AppendEntriesArguments {
             term,
             leader_id,
             prev_log_index,
@@ -27,17 +28,18 @@ impl Arguments {
     }
 }
 
-pub struct Results {
-    pub term: u8,
+#[derive(Clone, Debug)]
+pub struct AppendEntriesResults {
+    pub term: u32,
     pub success: bool,
 }
 
-impl Results {
-    pub async fn build() -> Result<Results, Box<dyn std::error::Error>> {
+impl AppendEntriesResults {
+    pub async fn build() -> Result<AppendEntriesResults, Box<dyn std::error::Error>> {
         let term = 0;
         let success = false;
 
-        Ok(Results { term, success })
+        Ok(AppendEntriesResults { term, success })
     }
 }
 
@@ -46,25 +48,25 @@ mod tests {
     use super::*;
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn arguments() -> Result<(), Box<dyn std::error::Error>> {
-        let test_arguments = Arguments::build().await?;
+    async fn append_entries_arguments() -> Result<(), Box<dyn std::error::Error>> {
+        let test_append_entries_arguments = AppendEntriesArguments::build().await?;
 
-        assert_eq!(test_arguments.term, 0);
-        assert_eq!(test_arguments.leader_id, "some_leader_id");
-        assert_eq!(test_arguments.prev_log_index, 0);
-        assert_eq!(test_arguments.prev_log_term, 0);
-        assert_eq!(test_arguments.entries.len(), 0);
-        assert_eq!(test_arguments.leader_commit, 0);
+        assert_eq!(test_append_entries_arguments.term, 0);
+        assert_eq!(test_append_entries_arguments.leader_id, "some_leader_id");
+        assert_eq!(test_append_entries_arguments.prev_log_index, 0);
+        assert_eq!(test_append_entries_arguments.prev_log_term, 0);
+        assert_eq!(test_append_entries_arguments.entries.len(), 0);
+        assert_eq!(test_append_entries_arguments.leader_commit, 0);
 
         Ok(())
     }
 
     #[tokio::test(flavor = "multi_thread")]
-    async fn results() -> Result<(), Box<dyn std::error::Error>> {
-        let test_results = Results::build().await?;
+    async fn append_entries_results() -> Result<(), Box<dyn std::error::Error>> {
+        let test_append_entries_results = AppendEntriesResults::build().await?;
 
-        assert_eq!(test_results.term, 0);
-        assert!(!test_results.success);
+        assert_eq!(test_append_entries_results.term, 0);
+        assert!(!test_append_entries_results.success);
 
         Ok(())
     }
