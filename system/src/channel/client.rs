fc84178ceab153301915b5ae5cf3c82400e77bb3
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-03 09:48:08 -0800

    initial client channel impl

---
+++ system/src/channel/client.rs
@@ -0,0 +1,49 @@
+use tokio::sync::{mpsc, oneshot};
+
+pub type ClientReceiver = mpsc::Receiver<(ClientRequest, oneshot::Sender<ClientResponse>)>;
+pub type ClientSender = mpsc::Sender<(ClientRequest, oneshot::Sender<ClientResponse>)>;
+
+#[derive(Clone, Debug)]
+pub enum ClientRequest {
+    // Candidate,
+    JoinCluster,
+    // Node,
+    PeerNodes,
+    PeerStatus,
+    StartElection,
+    SendHeartbeat,
+}
+
+#[derive(Clone, Debug)]
+pub enum ClientResponse {
+    JoinCluster,
+    // Node,
+    MemberNodes,
+    MemberStatus,
+}
+
+pub async fn start_election(client: &ClientSender) -> Result<(), Box<dyn std::error::Error>> {
+    let (request, _response) = oneshot::channel();
+
+    client.send((ClientRequest::StartElection, request)).await?;
+
+    // match response.await {
+    //     Ok(StateResponse::Candidate(request_vote_arguments)) => Ok(request_vote_arguments),
+    //     Err(error) => Err(Box::new(error)),
+    //     _ => panic!("unexpected response!"),
+    // }
+    Ok(())
+}
+
+pub async fn send_heartbeat(client: &ClientSender) -> Result<(), Box<dyn std::error::Error>> {
+    let (request, _response) = oneshot::channel();
+
+    client.send((ClientRequest::SendHeartbeat, request)).await?;
+
+    // match response.await {
+    //     Ok(StateResponse::Candidate(request_vote_arguments)) => Ok(request_vote_arguments),
+    //     Err(error) => Err(Box::new(error)),
+    //     _ => panic!("unexpected response!"),
+    // }
+    Ok(())
+}
