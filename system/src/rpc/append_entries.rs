03cffc8f1646de344c4015bed55f6ae07199f9bd
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-06 14:00:47 -0800

    remove impl, cleanup tests

--- system/src/rpc/append_entries.rs
+++ system/src/rpc/append_entries.rs
@@ -8,39 +8,10 @@ pub struct AppendEntriesArguments {
     pub leader_commit: u32,
 }
 
-impl AppendEntriesArguments {
-    pub async fn build() -> Result<AppendEntriesArguments, Box<dyn std::error::Error>> {
-        let term = 0;
-        let leader_id = String::from("some_leader_id");
-        let prev_log_index = 0;
-        let prev_log_term = 0;
-        let entries = Vec::with_capacity(10);
-        let leader_commit = 0;
-
-        Ok(AppendEntriesArguments {
-            term,
-            leader_id,
-            prev_log_index,
-            prev_log_term,
-            entries,
-            leader_commit,
-        })
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct AppendEntriesResults {
     pub term: u32,
     pub success: bool,
-}
-
-impl AppendEntriesResults {
-    pub async fn build() -> Result<AppendEntriesResults, Box<dyn std::error::Error>> {
-        let term = 0;
-        let success = false;
-
-        Ok(AppendEntriesResults { term, success })
-    }
 }
 
 #[cfg(test)]
@@ -49,7 +20,14 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn append_entries_arguments() -> Result<(), Box<dyn std::error::Error>> {
-        let test_append_entries_arguments = AppendEntriesArguments::build().await?;
+        let test_append_entries_arguments = AppendEntriesArguments {
+            term: 0,
+            leader_id: String::from("some_leader_id"),
+            prev_log_index: 0,
+            prev_log_term: 0,
+            entries: Vec::with_capacity(0),
+            leader_commit: 0,
+        };
 
         assert_eq!(test_append_entries_arguments.term, 0);
         assert_eq!(test_append_entries_arguments.leader_id, "some_leader_id");
@@ -63,7 +41,10 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn append_entries_results() -> Result<(), Box<dyn std::error::Error>> {
-        let test_append_entries_results = AppendEntriesResults::build().await?;
+        let test_append_entries_results = AppendEntriesResults {
+            term: 0,
+            success: false,
+        };
 
         assert_eq!(test_append_entries_results.term, 0);
         assert!(!test_append_entries_results.success);
--- system/src/rpc/request_vote.rs
+++ system/src/rpc/request_vote.rs
@@ -6,35 +6,10 @@ pub struct RequestVoteArguments {
     pub last_log_term: u32,
 }
 
-impl RequestVoteArguments {
-    pub async fn build() -> Result<RequestVoteArguments, Box<dyn std::error::Error>> {
-        let term = 0;
-        let candidate_id = String::from("some_candidate_id");
-        let last_log_index = 0;
-        let last_log_term = 0;
-
-        Ok(RequestVoteArguments {
-            term,
-            candidate_id,
-            last_log_index,
-            last_log_term,
-        })
-    }
-}
-
 #[derive(Clone, Debug)]
 pub struct RequestVoteResults {
     pub term: u32,
     pub vote_granted: bool,
-}
-
-impl RequestVoteResults {
-    pub async fn build() -> Result<RequestVoteResults, Box<dyn std::error::Error>> {
-        let term = 0;
-        let vote_granted = false;
-
-        Ok(RequestVoteResults { term, vote_granted })
-    }
 }
 
 #[cfg(test)]
@@ -43,7 +18,12 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn request_vote_arguments() -> Result<(), Box<dyn std::error::Error>> {
-        let test_request_vote_arguments = RequestVoteArguments::build().await?;
+        let test_request_vote_arguments = RequestVoteArguments {
+            term: 0,
+            candidate_id: String::from("some_candidate_id"),
+            last_log_index: 0,
+            last_log_term: 0,
+        };
 
         assert_eq!(test_request_vote_arguments.term, 0);
         assert_eq!(
@@ -58,7 +38,10 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread")]
     async fn request_vote_results() -> Result<(), Box<dyn std::error::Error>> {
-        let test_request_vote_results = RequestVoteResults::build().await?;
+        let test_request_vote_results = RequestVoteResults {
+            term: 0,
+            vote_granted: false,
+        };
 
         assert_eq!(test_request_vote_results.term, 0);
         assert!(!test_request_vote_results.vote_granted);
