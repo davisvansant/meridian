4d64f0827ee9e3cc1abf83845666c4827ef638f1
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-08 14:03:18 -0800

    add new heartbeat channel

--- system/src/channel/server.rs
+++ system/src/channel/server.rs
@@ -8,6 +8,9 @@ pub type ServerSender = broadcast::Sender<ServerState>;
 pub type CandidateReceiver = mpsc::Receiver<CandidateTransition>;
 pub type CandidateSender = mpsc::Sender<CandidateTransition>;
 
+pub type LeaderReceiver = mpsc::Receiver<Leader>;
+pub type LeaderSender = mpsc::Sender<Leader>;
+
 #[derive(Clone, Debug)]
 pub enum ServerState {
     Candidate,
@@ -21,4 +24,9 @@ pub enum ServerState {
 pub enum CandidateTransition {
     Follower,
     Leader,
+}
+
+#[derive(Clone, Debug)]
+pub enum Leader {
+    Heartbeat,
 }
