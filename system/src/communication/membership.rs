9410706974d8ad94749037b12647c56300b33040
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-01-11 10:28:12 -0800

    adjust multicast address and interface to init, add initial tests

--- system/src/communication/membership.rs
+++ system/src/communication/membership.rs
@@ -17,6 +17,8 @@ pub enum Messages {
 pub struct MembershipCommunication {
     socket_address: SocketAddr,
     buffer: [u8; 1024],
+    multicast_address: Ipv4Addr,
+    multicast_interface: Ipv4Addr,
 }
 
 impl MembershipCommunication {
@@ -24,20 +26,21 @@ impl MembershipCommunication {
         socket_address: SocketAddr,
     ) -> Result<MembershipCommunication, Box<dyn std::error::Error>> {
         let buffer = [0; 1024];
+        let multicast_address = Ipv4Addr::new(239, 0, 0, 1);
+        let multicast_interface = Ipv4Addr::from_str(socket_address.ip().to_string().as_str())?;
 
         Ok(MembershipCommunication {
             socket_address,
             buffer,
+            multicast_address,
+            multicast_interface,
         })
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let socket = UdpSocket::bind(self.socket_address).await?;
 
-        let multicast_address = Ipv4Addr::new(239, 0, 0, 1);
-        let interface = Ipv4Addr::new(0, 0, 0, 0);
-
-        socket.join_multicast_v4(multicast_address, interface)?;
+        socket.join_multicast_v4(self.multicast_address, self.multicast_interface)?;
 
         loop {
             let (bytes, origin) = socket.recv_from(&mut self.buffer).await?;
@@ -53,6 +56,30 @@ impl MembershipCommunication {
                 String::from_utf8(self.buffer[..bytes].to_vec())?,
             );
         }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init() -> Result<(), Box<dyn std::error::Error>> {
+        let test_socket_address = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 8888);
+        let test_membership_communication =
+            MembershipCommunication::init(test_socket_address).await?;
+
+        assert!(test_membership_communication.socket_address.ip().is_ipv4());
+        assert_eq!(test_membership_communication.buffer, [0_u8; 1024]);
+        assert!(test_membership_communication
+            .multicast_address
+            .is_multicast());
+        assert_eq!(
+            test_membership_communication.multicast_interface,
+            Ipv4Addr::UNSPECIFIED,
+        );
 
         Ok(())
     }
