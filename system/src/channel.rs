265b1a2cf40e063c810d0d89b8f3d0ef72c355b0
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-02 15:01:56 -0800

    add initial membership maintenance channel impl

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -1,5 +1,6 @@
 mod client;
 mod membership;
+mod membership_maintenance;
 mod rpc_server;
 mod server;
 mod state;
@@ -8,6 +9,10 @@ pub use client::{ClientReceiver, ClientRequest, ClientResponse, ClientSender};
 pub use membership::{
     MembershipDynamicJoinShutdown, MembershipReceiver, MembershipRequest, MembershipResponse,
     MembershipSender,
+};
+pub use membership_maintenance::{
+    MembershipMaintenanceReceiver, MembershipMaintenanceRequest, MembershipMaintenanceResponse,
+    MembershipMaintenanceSender, MembershipMaintenanceShutdown,
 };
 pub use rpc_server::RpcServerShutdown;
 pub use server::{CandidateReceiver, CandidateSender, CandidateTransition};
@@ -21,4 +26,5 @@ pub use client::{
 pub use membership::{
     add_member, cluster_members, get_node, launch_nodes, shutdown_membership, status,
 };
+pub use membership_maintenance::shutdown_membership_maintenance;
 pub use state::{append_entries, candidate, heartbeat, leader, request_vote, shutdown_state};
---
+++ system/src/channel/membership_maintenance.rs
@@ -0,0 +1,33 @@
+use std::net::SocketAddr;
+use tokio::sync::{broadcast, mpsc, oneshot};
+
+pub type MembershipMaintenanceShutdown = mpsc::Receiver<bool>;
+pub type MembershipMaintenanceReceiver = mpsc::Receiver<(
+    MembershipMaintenanceRequest,
+    oneshot::Sender<MembershipMaintenanceResponse>,
+)>;
+pub type MembershipMaintenanceSender = mpsc::Sender<(
+    MembershipMaintenanceRequest,
+    oneshot::Sender<MembershipMaintenanceResponse>,
+)>;
+
+#[derive(Clone, Debug)]
+pub enum MembershipMaintenanceRequest {
+    Message((Vec<u8>, SocketAddr)),
+    Shutdown,
+}
+
+#[derive(Clone, Debug)]
+pub enum MembershipMaintenanceResponse {}
+
+pub async fn shutdown_membership_maintenance(
+    membership_maintenance: &MembershipMaintenanceSender,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let (request, _response) = oneshot::channel();
+
+    membership_maintenance
+        .send((MembershipMaintenanceRequest::Shutdown, request))
+        .await?;
+
+    Ok(())
+}
