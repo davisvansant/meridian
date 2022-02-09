65507446b253adbe6dd5d4ef2543b349e6915692
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-09 13:23:50 -0800

    cleanup, update tests

--- system/src/server.rs
+++ system/src/server.rs
@@ -1,9 +1,9 @@
 use tokio::signal::unix::{signal, SignalKind};
-// use tokio::sync::{broadcast, mpsc, oneshot};
-// use tokio::sync::broadcast;
-// use tokio::time::{sleep, timeout, timeout_at, Duration, Instant};
 use tokio::time::{sleep, Duration};
 
+use crate::channel::LeaderSender;
+use crate::channel::{CandidateSender, CandidateTransition};
+use crate::channel::{ClientSender, MembershipSender, StateSender};
 use crate::server::candidate::Candidate;
 use crate::server::follower::Follower;
 use crate::server::leader::Leader;
@@ -12,16 +12,6 @@ pub mod candidate;
 pub mod follower;
 pub mod leader;
 mod preflight;
-
-// use crate::channel::{CandidateReceiver, CandidateSender, CandidateTransition};
-use crate::channel::{CandidateSender, CandidateTransition};
-use crate::channel::{ClientSender, MembershipSender, StateSender};
-// use crate::channel::{LeaderReceiver, LeaderSender};
-// use crate::channel::LeaderReceiver;
-use crate::channel::LeaderSender;
-// use crate::channel::{
-//     SendServerShutdown, ServerReceiver, ServerSender, ServerShutdown, ServerState,
-// };
 
 pub enum ServerState {
     Candidate,
@@ -35,30 +25,18 @@ pub struct Server {
     pub server_state: ServerState,
     client: ClientSender,
     membership: MembershipSender,
-    // receiver: ServerReceiver,
     state: StateSender,
-    // tx: ServerSender,
-    // rx: ServerReceiver,
     candidate_sender: CandidateSender,
-    // candidate_receiver: CandidateReceiver,
-    // heartbeat: LeaderReceiver,
     heartbeat: LeaderSender,
-    // shutdown: ServerShutdown,
 }
 
 impl Server {
     pub async fn init(
         client: ClientSender,
         membership: MembershipSender,
-        // receiver: ServerReceiver,
         state: StateSender,
-        // tx: ServerSender,
-        // rx: ServerReceiver,
         candidate_sender: CandidateSender,
-        // candidate_receiver: CandidateReceiver,
-        // heartbeat: LeaderReceiver,
         heartbeat: LeaderSender,
-        // shutdown: ServerShutdown,
     ) -> Result<Server, Box<dyn std::error::Error>> {
         let server_state = ServerState::Follower;
 
@@ -66,22 +44,13 @@ impl Server {
             server_state,
             client,
             membership,
-            // receiver,
             state,
-            // tx,
-            // rx,
             candidate_sender,
-            // candidate_receiver,
             heartbeat,
-            // shutdown,
         })
     }
 
-    pub async fn run(
-        &mut self,
-        // test_heartbeat: LeaderSender,
-        // candidate_receiver: CandidateSender,
-    ) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         sleep(Duration::from_secs(5)).await;
 
         let mut stream = signal(SignalKind::interrupt())?;
@@ -134,11 +103,7 @@ impl Server {
                 let mut candidate_receiver = self.candidate_sender.subscribe();
                 let mut candidate = Candidate::init().await?;
 
-                match candidate
-                    // .run(&self.client, &mut self.candidate_receiver, &self.tx)
-                    .run(&self.client, &mut candidate_receiver)
-                    .await
-                {
+                match candidate.run(&self.client, &mut candidate_receiver).await {
                     Ok(CandidateTransition::Follower) => {
                         self.server_state = ServerState::Follower;
                     }
@@ -178,7 +143,6 @@ mod tests {
     use super::*;
     use crate::channel::CandidateTransition;
     use crate::channel::Leader;
-    // use crate::channel::ServerState;
     use crate::channel::{ClientRequest, ClientResponse};
     use crate::channel::{MembershipRequest, MembershipResponse};
     use crate::channel::{StateRequest, StateResponse};
@@ -192,38 +156,24 @@ mod tests {
             mpsc::channel::<(MembershipRequest, oneshot::Sender<MembershipResponse>)>(64);
         let (test_state_sender, _test_state_receiver) =
             mpsc::channel::<(StateRequest, oneshot::Sender<StateResponse>)>(64);
-        // let (test_transition_sender, test_transition_receiver) =
-        //     broadcast::channel::<ServerState>(64);
-        // let (test_transition_sender, test_transition_receiver) = mpsc::channel::<ServerState>(64);
-        // let (test_candidate_sender, test_candidate_receiver) =
-        //     mpsc::channel::<CandidateTransition>(64);
         let (test_candidate_sender, _test_candidate_receiver) =
             broadcast::channel::<CandidateTransition>(64);
-        // let (test_leader_sender, test_leader_receiver) = mpsc::channel::<Leader>(64);
         let (test_leader_sender, _test_leader_receiver) = broadcast::channel::<Leader>(64);
-        // let (test_shutdown_sender, test_shutdown_receiver) = mpsc::channel::<()>(1);
 
         let test_server = Server::init(
             test_client_sender,
             test_membership_sender,
             test_state_sender,
-            // test_transition_sender,
-            // test_transition_receiver,
             test_candidate_sender,
-            // test_candidate_receiver,
             test_leader_sender,
-            // test_leader_receiver,
-            // test_shutdown_receiver,
         )
         .await?;
 
         assert!(!test_server.client.is_closed());
         assert!(!test_server.membership.is_closed());
         assert!(!test_server.state.is_closed());
-        // assert_eq!(test_server.tx.receiver_count(), 1);
-        // assert!(!test_server.candidate_sender.is_closed());
-        // assert!(!test_leader_sender.is_closed());
-        // assert!(!test_shutdown_sender.is_closed());
+        assert_eq!(test_server.candidate_sender.receiver_count(), 1);
+        assert_eq!(test_server.heartbeat.receiver_count(), 1);
 
         Ok(())
     }
