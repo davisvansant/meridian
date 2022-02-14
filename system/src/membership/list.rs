cd151f23eec9166abc5f2cb5cddab30123cf762e
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-14 13:09:52 -0800

    add initial insert/remove for alive

--- system/src/membership/list.rs
+++ system/src/membership/list.rs
@@ -6,8 +6,8 @@ use uuid::Uuid;
 use crate::node::Node;
 
 pub struct List {
-    initial: Vec<SocketAddr>,
-    alive: HashMap<Uuid, Node>,
+    pub initial: Vec<SocketAddr>,
+    pub alive: HashMap<Uuid, Node>,
     suspected: HashMap<Uuid, Node>,
     confirmed: HashMap<Uuid, Node>,
 }
@@ -24,5 +24,22 @@ impl List {
             suspected,
             confirmed,
         })
+    }
+
+    pub async fn insert_alive(&mut self, node: Node) -> Result<(), Box<dyn std::error::Error>> {
+        match self.alive.insert(node.id, node) {
+            Some(value) => println!("updated node! {:?}", value),
+            None => println!("added node !"),
+        }
+
+        Ok(())
+    }
+
+    pub async fn remove_alive(&mut self, node: &Node) -> Result<(), Box<dyn std::error::Error>> {
+        if let Some(remove_alive) = self.alive.remove(&node.id) {
+            println!("removed from alive group - > {:?}", remove_alive);
+        }
+
+        Ok(())
     }
 }
