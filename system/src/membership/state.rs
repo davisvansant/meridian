411b43699d3f84e56db4e025f63b1e78dc583991
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-14 11:22:16 -0800

    remove state, adjusted to list

--- system/src/membership/state.rs
+++
@@ -1,7 +0,0 @@
-pub struct MembershipState {
-    cluster_size: ClusterSize,
-    server: Node,
-    launch_nodes: Vec<SocketAddr>,
-    members: HashMap<Uuid, Node>,
-    receiver: MembershipReceiver,
-}
