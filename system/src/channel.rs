b157c64eba1507933c646c6e2f6d4b88e676c8a4
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-16 13:05:51 -0800

    start to cleanup communication mod

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -2,7 +2,7 @@
 mod membership;
 mod membership_communications;
 mod membership_list;
-mod membership_maintenance;
+// mod membership_maintenance;
 // mod rpc_server;
 mod rpc_client;
 mod server;
@@ -20,10 +20,10 @@ pub use membership_communications::{
 pub use membership_list::{
     MembershipListReceiver, MembershipListRequest, MembershipListResponse, MembershipListSender,
 };
-pub use membership_maintenance::{
-    MembershipMaintenanceReceiver, MembershipMaintenanceRequest, MembershipMaintenanceResponse,
-    MembershipMaintenanceSender, MembershipMaintenanceShutdown,
-};
+// pub use membership_maintenance::{
+//     MembershipMaintenanceReceiver, MembershipMaintenanceRequest, MembershipMaintenanceResponse,
+//     MembershipMaintenanceSender, MembershipMaintenanceShutdown,
+// };
 // pub use rpc_client::{ClientReceiver, ClientRequest, ClientResponse, ClientSender};
 pub use rpc_client::{RpcClientReceiver, RpcClientRequest, RpcClientResponse, RpcClientSender};
 pub use server::{CandidateReceiver, CandidateSender, CandidateTransition};
@@ -36,7 +36,7 @@ pub use state::{StateReceiver, StateRequest, StateResponse, StateSender};
 // pub use client::{send_heartbeat, shutdown_client, start_election};
 pub use membership::{cluster_members, get_node, shutdown_membership, status};
 pub use membership_list::get_alive;
-pub use membership_maintenance::shutdown_membership_maintenance;
+// pub use membership_maintenance::shutdown_membership_maintenance;
 // pub use rpc_client::{send_heartbeat, shutdown_client, start_election};
 pub use rpc_client::{send_heartbeat, shutdown_rpc_client, start_election};
 pub use state::{append_entries, candidate, heartbeat, leader, request_vote, shutdown_state};
--- system/src/lib.rs
+++ system/src/lib.rs
@@ -1,5 +1,5 @@
 pub(crate) mod channel;
-pub mod communication;
+// pub mod communication;
 // pub(crate) mod grpc;
 pub(crate) mod membership;
 pub mod node;
