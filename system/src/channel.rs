f5b73bd86d92a817e725e69b27b842474a58def2
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-17 14:21:55 -0800

    update pub use with membership list actions

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -20,6 +20,9 @@ pub use state::{StateReceiver, StateRequest, StateResponse, StateSender};
 
 pub use membership::{cluster_members, get_node, shutdown_membership, status};
 pub use membership_communications::send_message;
-pub use membership_list::{get_alive, get_initial, shutdown_membership_list};
+pub use membership_list::{
+    get_alive, get_confirmed, get_initial, get_suspected, insert_alive, insert_confirmed,
+    insert_suspected, remove_alive, remove_confirmed, remove_suspected, shutdown_membership_list,
+};
 pub use rpc_client::{send_heartbeat, shutdown_rpc_client, start_election};
 pub use state::{append_entries, candidate, heartbeat, leader, request_vote, shutdown_state};
