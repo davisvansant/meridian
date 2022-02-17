4ef3e21433a9c9f54ba7c03179273da5c6190855
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-02-17 13:42:36 -0800

    remove pub type, cleanup

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -1,18 +1,11 @@
-// mod client;
 mod membership;
 mod membership_communications;
 mod membership_list;
-// mod membership_maintenance;
-// mod rpc_server;
 mod rpc_client;
 mod server;
 mod state;
 
-// pub use client::{ClientReceiver, ClientRequest, ClientResponse, ClientSender};
-pub use membership::{
-    MembershipDynamicJoinShutdown, MembershipReceiver, MembershipRequest, MembershipResponse,
-    MembershipSender,
-};
+pub use membership::{MembershipReceiver, MembershipRequest, MembershipResponse, MembershipSender};
 pub use membership_communications::{
     MembershipCommunicationsMessage, MembershipCommunicationsReceiver,
     MembershipCommunicationsSender,
@@ -20,25 +13,13 @@ pub use membership_communications::{
 pub use membership_list::{
     MembershipListReceiver, MembershipListRequest, MembershipListResponse, MembershipListSender,
 };
-// pub use membership_maintenance::{
-//     MembershipMaintenanceReceiver, MembershipMaintenanceRequest, MembershipMaintenanceResponse,
-//     MembershipMaintenanceSender, MembershipMaintenanceShutdown,
-// };
-// pub use rpc_client::{ClientReceiver, ClientRequest, ClientResponse, ClientSender};
-// pub use rpc_client::{RpcClientReceiver, RpcClientRequest, RpcClientResponse, RpcClientSender};
 pub use rpc_client::{RpcClientReceiver, RpcClientRequest, RpcClientSender};
 pub use server::{CandidateReceiver, CandidateSender, CandidateTransition};
 pub use server::{Leader, LeaderReceiver, LeaderSender};
 pub use state::{StateReceiver, StateRequest, StateResponse, StateSender};
 
-// pub use client::{
-//     join_cluster, peer_nodes, peer_status, send_heartbeat, shutdown_client, start_election,
-// };
-// pub use client::{send_heartbeat, shutdown_client, start_election};
 pub use membership::{cluster_members, get_node, shutdown_membership, status};
 pub use membership_communications::send_message;
 pub use membership_list::{get_alive, get_initial, shutdown_membership_list};
-// pub use membership_maintenance::shutdown_membership_maintenance;
-// pub use rpc_client::{send_heartbeat, shutdown_client, start_election};
 pub use rpc_client::{send_heartbeat, shutdown_rpc_client, start_election};
 pub use state::{append_entries, candidate, heartbeat, leader, request_vote, shutdown_state};
