c48c5ced88fb4f833a5cf725dba0cdb21e4ce753
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-03-01 13:21:36 -0800

    add initial shutdown channel, impl

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -3,6 +3,7 @@ mod membership_communications;
 mod membership_list;
 mod rpc_client;
 mod server;
+mod shutdown;
 mod state;
 
 pub use membership::{MembershipReceiver, MembershipRequest, MembershipResponse, MembershipSender};
@@ -15,6 +16,7 @@ pub use membership_list::{
 pub use rpc_client::{RpcClientReceiver, RpcClientRequest, RpcClientSender};
 pub use server::{CandidateReceiver, CandidateSender, CandidateTransition};
 pub use server::{Leader, LeaderReceiver, LeaderSender};
+pub use shutdown::{ShutdownReceiver, ShutdownSender};
 pub use state::{StateReceiver, StateRequest, StateResponse, StateSender};
 
 pub use membership::{cluster_members, get_node, shutdown_membership, static_join};
@@ -24,4 +26,5 @@ pub use membership_list::{
     insert_suspected, remove_alive, remove_confirmed, remove_suspected, shutdown_membership_list,
 };
 pub use rpc_client::{send_heartbeat, shutdown_rpc_client, start_election};
+pub use shutdown::{build_shutdown_channel, shutdown};
 pub use state::{append_entries, candidate, heartbeat, leader, request_vote, shutdown_state};
---
+++ system/src/channel/shutdown.rs
@@ -0,0 +1,16 @@
+use tokio::sync::broadcast;
+
+pub type ShutdownReceiver = broadcast::Receiver<bool>;
+pub type ShutdownSender = broadcast::Sender<bool>;
+
+pub async fn build_shutdown_channel() -> ShutdownSender {
+    let (shutdown_sender, _rx) = broadcast::channel(64);
+
+    shutdown_sender
+}
+
+pub async fn shutdown(shutdown: &ShutdownSender) -> Result<(), Box<dyn std::error::Error>> {
+    shutdown.send(false)?;
+
+    Ok(())
+}
