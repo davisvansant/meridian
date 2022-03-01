7859f5382a1302b62e952ca9a800d251ec14d55e
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-03-01 13:51:09 -0800

    remove current grpc impl

--- system/src/grpc.rs
+++
@@ -1,8 +0,0 @@
-pub(self) use tonic::transport::Channel;
-pub(self) use tonic::{Code, Request, Response, Status};
-
-pub mod client_server;
-pub mod cluster_client;
-pub mod cluster_server;
-pub mod membership_client;
-pub mod membership_server;
