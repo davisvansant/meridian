efbcec8f182517b69f60b5a98ba5c3fb301a7670
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-03-03 15:50:11 -0800

    adjust how static join is ran, utilizing channels to build message

--- system/src/membership/static_join.rs
+++ system/src/membership/static_join.rs
@@ -3,6 +3,11 @@ use crate::channel::MembershipListSender;
 use crate::channel::{get_initial, send_message};
 use crate::membership::Message;
 
+use crate::channel::get_alive;
+use crate::channel::get_confirmed;
+use crate::channel::get_node;
+use crate::channel::get_suspected;
+
 pub struct StaticJoin {
     membership_communications_sender: MembershipCommunicationsSender,
     membership_list_sender: MembershipListSender,
@@ -20,12 +25,27 @@ impl StaticJoin {
     }
 
     pub async fn run(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let node = get_node(&self.membership_list_sender).await?;
+        let alive_list = get_alive(&self.membership_list_sender).await?;
+        let suspected_list = get_suspected(&self.membership_list_sender).await?;
+        let confirmed_list = get_confirmed(&self.membership_list_sender).await?;
         let initial_nodes = get_initial(&self.membership_list_sender).await?;
+        let ping = Message::Ping
+            .build_list(node, alive_list, suspected_list, confirmed_list)
+            .await;
 
         for origin in initial_nodes {
-            let ping = Message::Ping.build().await.to_vec();
+            // let ping = Message::Ping.build().await.to_vec();
+            // let node = get_node(&self.membership_list_sender).await?;
+            // let alive_list = get_alive(&self.membership_list_sender).await?;
+            // let suspected_list = get_suspected(&self.membership_list_sender).await?;
+            // let confirmed_list = get_confirmed(&self.membership_list_sender).await?;
 
-            send_message(&self.membership_communications_sender, ping, origin).await?;
+            // let ping = Message::Ping
+            //     .build_list(node, alive_list, suspected_list, confirmed_list)
+            //     .await;
+
+            send_message(&self.membership_communications_sender, &ping, origin).await?;
         }
 
         Ok(())
