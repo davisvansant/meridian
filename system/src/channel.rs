445f8c96df554a6ac5a5fc02abd11be785c27637
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-06-15 12:44:59 -0700

    remove rpc_client and shutdown channels

--- system/src/channel.rs
+++ system/src/channel.rs
@@ -2,7 +2,6 @@ pub mod membership;
 pub mod membership_communications;
 pub mod membership_failure_detector;
 pub mod membership_list;
-pub mod rpc_client;
 pub mod server;
-pub mod shutdown;
 pub mod state;
+pub mod transition;
