5c8b2288d83e09479c092ef88fc40aa323f0bcdb
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-06-21 11:56:54 -0700

    add suspected/confirmed on receiving pings

--- system/src/membership/communications.rs
+++ system/src/membership/communications.rs
@@ -185,6 +185,18 @@ impl MembershipCommunications {
                 for alive_node in &peer_active_list {
                     insert_alive(list_sender, alive_node).await?;
                 }
+
+                for suspected_node in &peer_suspected_list {
+                    remove_alive(list_sender, suspected_node).await?;
+                    // remove_confirmed(list_sender, suspected_node).await?;
+                    insert_suspected(list_sender, suspected_node).await?;
+                }
+
+                for confirmed_node in &peer_confirmed_list {
+                    remove_alive(list_sender, confirmed_node).await?;
+                    // remove_suspected(list_sender, confirmed_node).await?;
+                    insert_confirmed(list_sender, confirmed_node).await?;
+                }
             }
             Message::PingReq => {
                 info!("received ping request!");
