53768cbada198a467ca999ba9c4d87c14090f530
Davis Van Sant <davisvansant@users.noreply.github.com>
2022-06-27 12:29:39 -0700

    cleanup, add tests

--- system/src/membership.rs
+++ system/src/membership.rs
@@ -72,12 +72,14 @@ impl Membership {
         launch_nodes: Vec<SocketAddr>,
     ) -> Result<(), Box<dyn std::error::Error>> {
         let (list_sender, list_receiver) = crate::channel::membership_list::build().await;
-        let static_join_send_list = list_sender.to_owned();
-        let communications_list_sender = list_sender.to_owned();
-        let failure_detector_list_sender = list_sender.to_owned();
-
         let failure_detector_ping_target_sender = build_ping_target().await;
-        let communications_ping_target_sender = failure_detector_ping_target_sender.to_owned();
+        let send_udp_message = crate::channel::membership_communications::build().await;
+        let (failure_detector_sender, failure_detector_reciever) = build().await;
+
+        let mut communications_shutdown = self.shutdown.subscribe();
+        let mut failure_detector_shutdown = self.shutdown.subscribe();
+
+        drop(self.shutdown.to_owned());
 
         let mut list = List::init(server, launch_nodes, list_receiver).await?;
 
@@ -87,25 +89,14 @@ impl Membership {
             }
         });
 
-        let send_udp_message = crate::channel::membership_communications::build().await;
-        let membership_communications_sender = send_udp_message.clone();
-        let static_join_send_udp_message = send_udp_message.clone();
-        let failure_detector_send_udp_message = send_udp_message.clone();
-
         let membership_port = server.membership_address().await;
-
         let mut communications = MembershipCommunications::init(
             membership_port,
-            communications_list_sender,
-            membership_communications_sender,
-            communications_ping_target_sender,
+            list_sender.to_owned(),
+            send_udp_message.to_owned(),
+            failure_detector_ping_target_sender.to_owned(),
         )
         .await;
-
-        let mut communications_shutdown = self.shutdown.subscribe();
-        let mut failure_detector_shutdown = self.shutdown.subscribe();
-
-        drop(self.shutdown.to_owned());
 
         tokio::spawn(async move {
             if let Err(error) = communications.run(&mut communications_shutdown).await {
@@ -113,12 +104,10 @@ impl Membership {
             }
         });
 
-        let (failure_detector_sender, failure_detector_reciever) = build().await;
-
         let mut failure_detector = FailureDectector::init(
-            failure_detector_list_sender,
+            list_sender.to_owned(),
             failure_detector_reciever,
-            failure_detector_send_udp_message,
+            send_udp_message.to_owned(),
             failure_detector_ping_target_sender,
         )
         .await;
@@ -130,7 +119,7 @@ impl Membership {
         });
 
         let mut static_join =
-            StaticJoin::init(static_join_send_udp_message, static_join_send_list).await;
+            StaticJoin::init(send_udp_message.to_owned(), list_sender.to_owned()).await;
 
         info!("running...");
 
@@ -195,57 +184,73 @@ impl Membership {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use std::str::FromStr;
-//     use tokio::sync::{mpsc, oneshot};
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[tokio::test(flavor = "multi_thread")]
-//     async fn init_one() -> Result<(), Box<dyn std::error::Error>> {
-//         let test_launch_nodes = Vec::with_capacity(0);
-//         let test_ip_address = SocketAddr::from_str("0.0.0.0:0")?.ip();
-//         let test_server = Node::init(test_ip_address, 1000, 2000, 3000).await?;
-//         let (test_tx, test_rx) =
-//             mpsc::channel::<(MembershipRequest, oneshot::Sender<MembershipResponse>)>(64);
-//         let test_membership =
-//             Membership::init(ClusterSize::One, test_launch_nodes, test_server, test_rx).await?;
-//         assert_eq!(test_membership.cluster_size, ClusterSize::One);
-//         assert_eq!(test_membership.members.len(), 0);
-//         assert!(test_membership.members.capacity() >= 1);
-//         assert!(!test_tx.is_closed());
-//         Ok(())
-//     }
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cluster_size_one() -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(ClusterSize::from_str("1").await, ClusterSize::One);
+        assert_eq!(ClusterSize::One.majority().await, 0);
 
-//     #[tokio::test(flavor = "multi_thread")]
-//     async fn init_three() -> Result<(), Box<dyn std::error::Error>> {
-//         let test_launch_nodes = Vec::with_capacity(0);
-//         let test_ip_address = SocketAddr::from_str("0.0.0.0:0")?.ip();
-//         let test_server = Node::init(test_ip_address, 1000, 2000, 3000).await?;
-//         let (test_tx, test_rx) =
-//             mpsc::channel::<(MembershipRequest, oneshot::Sender<MembershipResponse>)>(64);
-//         let test_membership =
-//             Membership::init(ClusterSize::Three, test_launch_nodes, test_server, test_rx).await?;
-//         assert_eq!(test_membership.cluster_size, ClusterSize::Three);
-//         assert_eq!(test_membership.members.len(), 0);
-//         assert!(test_membership.members.capacity() >= 3);
-//         assert!(!test_tx.is_closed());
-//         Ok(())
-//     }
+        Ok(())
+    }
 
-//     #[tokio::test(flavor = "multi_thread")]
-//     async fn init_five() -> Result<(), Box<dyn std::error::Error>> {
-//         let test_launch_nodes = Vec::with_capacity(0);
-//         let test_ip_address = SocketAddr::from_str("0.0.0.0:0")?.ip();
-//         let test_server = Node::init(test_ip_address, 1000, 2000, 3000).await?;
-//         let (test_tx, test_rx) =
-//             mpsc::channel::<(MembershipRequest, oneshot::Sender<MembershipResponse>)>(64);
-//         let test_membership =
-//             Membership::init(ClusterSize::Five, test_launch_nodes, test_server, test_rx).await?;
-//         assert_eq!(test_membership.cluster_size, ClusterSize::Five);
-//         assert_eq!(test_membership.members.len(), 0);
-//         assert!(test_membership.members.capacity() >= 5);
-//         assert!(!test_tx.is_closed());
-//         Ok(())
-//     }
-// }
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cluster_size_three() -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(ClusterSize::from_str("3").await, ClusterSize::Three);
+        assert_eq!(ClusterSize::Three.majority().await, 2);
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cluster_size_five() -> Result<(), Box<dyn std::error::Error>> {
+        assert_eq!(ClusterSize::from_str("5").await, ClusterSize::Five);
+        assert_eq!(ClusterSize::Five.majority().await, 3);
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init_one() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_sender, test_receiver) = crate::channel::membership::build().await;
+        let test_shutdown_signal = crate::channel::transition::Shutdown::build().await;
+        let test_membership =
+            Membership::init(ClusterSize::One, test_receiver, test_shutdown_signal).await?;
+
+        assert_eq!(test_membership.cluster_size, ClusterSize::One);
+        assert_eq!(test_sender.capacity(), 64);
+        assert_eq!(test_membership.shutdown.receiver_count(), 0);
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init_three() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_sender, test_receiver) = crate::channel::membership::build().await;
+        let test_shutdown_signal = crate::channel::transition::Shutdown::build().await;
+        let test_membership =
+            Membership::init(ClusterSize::Three, test_receiver, test_shutdown_signal).await?;
+
+        assert_eq!(test_membership.cluster_size, ClusterSize::Three);
+        assert_eq!(test_sender.capacity(), 64);
+        assert_eq!(test_membership.shutdown.receiver_count(), 0);
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn init_five() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_sender, test_receiver) = crate::channel::membership::build().await;
+        let test_shutdown_signal = crate::channel::transition::Shutdown::build().await;
+        let test_membership =
+            Membership::init(ClusterSize::Five, test_receiver, test_shutdown_signal).await?;
+
+        assert_eq!(test_membership.cluster_size, ClusterSize::Five);
+        assert_eq!(test_sender.capacity(), 64);
+        assert_eq!(test_membership.shutdown.receiver_count(), 0);
+
+        Ok(())
+    }
+}
